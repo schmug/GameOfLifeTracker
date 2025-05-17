7d05b000a5eff3d54b1f94f943bd0ce9886bd1f4
schmug <38227427+schmug@users.noreply.github.com>
2025-05-17 10:07:16 -0400

    fix routing base path

--- client/src/GitHubPagesApp.tsx
+++ client/src/GitHubPagesApp.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router as WouterRouter } from "wouter";
 import { useState, useEffect } from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
@@ -9,10 +9,12 @@ import NotFound from "./pages/not-found";
 function AppContent() {
   return (
     <main className="flex flex-col min-h-screen bg-background">
-      <Switch>
-        <Route path="/" component={StaticHome} />
-        <Route component={NotFound} />
-      </Switch>
+      <WouterRouter base={import.meta.env.BASE_URL}>
+        <Switch>
+          <Route path="/" component={StaticHome} />
+          <Route component={NotFound} />
+        </Switch>
+      </WouterRouter>
       <Toaster />
     </main>
   );
